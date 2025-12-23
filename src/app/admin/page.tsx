ff45efdb471322e2afca735b0e3917c8b4e71803
ainalisadata-netizen <ainalisadata@gmail.com>
2025-12-23 08:36:58 +0700

    repair error deploy vercel 5

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import Image from 'next/image'
 import { useAuth } from '@/hooks/useAuth'
 import { createClient } from '@/lib/supabase/client'
 
